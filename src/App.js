a3755814b15854e5f001664d35a70db895a1057d
Sagar Shiroya <sagar@alphacode.global>
2021-12-01 13:42:17 +0530

    Added about component,commented TextForm component, add state for toggle dark/light mode in about component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
@@ -10,7 +11,8 @@ function App() {
         {/* <Navbar /> */}
         <Navbar title="TextUtils"/>
         <div className="container my-3">
-          <TextForm heading="Enter the text to analyze below"/>
+          {/* <TextForm heading="Enter the text to analyze below"/> */}
+          <About/>
         </div>
 
       </>
