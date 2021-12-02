26c6b2e19a7d931857ccc60c64986f88d4ff2d32
Sagar Shiroya <sagar.shiroya8@gmail.com>
2021-12-02 16:00:36 +0530

    Changed the icon, added routes, remove old favicon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import {useState} from 'react';
 import Alert from './components/Alert';
+import React, {Fragment} from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
 
 function App() {
 
   const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
+  // const [bgColor, setBgColor] = useState('white')
 
   const showAlert = (message, type)=>{
     setAlert({
@@ -23,7 +31,7 @@ function App() {
   const toggleMode = ()=>{
     if(mode === 'light') {
       setMode('dark');
-      document.body.style.backgroundColor = '#042743';
+      document.body.style.backgroundColor = 'black';
       showAlert('Dark Mode Enabled', 'success');
     } else {
       setMode('light');
@@ -31,18 +39,34 @@ function App() {
       showAlert('Light Mode Enabled', 'success');
     }
   }
-  return (
-      <>
-        {/* Different kind of props passed to the Navbar component */}
-        {/* <Navbar /> */}
-        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
-        <div className="container my-3">
-          <Alert alert={alert}/>
-          <TextForm heading="Enter the text to analyze below" showAlert={showAlert} mode={mode} />
-          {/* <About/> */}
-        </div>
 
-      </>
+  const changeBgColor = (color)=>{
+    document.body.style.backgroundColor = color;
+  }
+  return (
+      
+      
+        <>
+          {/* Different kind of props passed to the Navbar component */}
+          {/* <Navbar /> */}
+          <Router>
+            <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} changeBgColor={changeBgColor}/>
+            <div className="container my-3">
+            <Alert alert={alert}/>
+            <Switch>
+              {/* Always use exact path, otherwise react router try to match partially.  */}
+              <Route exact path="/about">
+                <About />
+              </Route>
+              <Route exact path="/">
+                <TextForm heading="Enter the text to analyze below" showAlert={showAlert} mode={mode} />
+              </Route>
+            </Switch>
+            </div>
+          </Router>
+        </>
+        
+      
   );
 }
 
